4a478753c2b57037667c12762b83bb63463f69a3
Rainer Krüger <rainer@maguis.com.br>
2017-10-08 00:54:00 +0200

    Removendo packages da distro

--- packages/pandorga-base/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pandorga-base):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pandorga-base" command="/usr/bin/pandorga-base"
--- packages/pandorga-base/debian/pandorga-base.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pandorga-base package
-#
-0 4	* * *	root	[ -x /usr/bin/pandorga-base_maintenance ] && /usr/bin/pandorga-base_maintenance
